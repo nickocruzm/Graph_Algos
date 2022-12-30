06bbbbec40655e2894eed8fd298d5e534120d0ea
Nicko Cruz M <nickocruzm@gmail.com>
2022-12-30 13:32:37 -0800

    BellmanFord start

--- BellmanFord.cpp
+++ BellmanFord.cpp
@@ -1,104 +1,73 @@
-#include <iostream>
-#include <limits>
+#include "BellmanFord.hpp"
 
-using namespace std;
-// Number of vertices in the graph
-#define V 9
+int BellmanFord(Graph& G, int s, int f){
+    int N = G.v_count();
+    int M = G.e_count();
+    vector<int> T_dist(N,inf);
+    // if(f<s){
+    //     int t = s;
+    //     s = f;
+    //     f = t;
 
-// A utility function to find the vertex with minimum
-// distance value, from the set of vertices not yet included
-// in shortest path tree
-int minDistance(int dist[], bool sptSet[])
-{
+    // }
+    T_dist[s] = 0;
 
-	// Initialize min value
-	int min = INT_MAX, min_index;
+    int su = s;
+    for(int i = 0; i < N; i++){
+        int ui = G.E[i].u;
+        int vi = G.E[i].v;
 
-	for (int v = 0; v < V; v++)
-		if (sptSet[v] == false && dist[v] <= min)
-			min = dist[v], min_index = v;
+        if(su != ui){
+            continue;
+        }
 
-	return min_index;
+        else{ 
+            T_dist[vi] = T_dist[ui] + G.E[i].w;
+        }
+
+
+    }
+
+
+    cout<< endl;
+    for(int i = 0; i < T_dist.size(); i++){
+        cout<< "i: "<<i << " "<< T_dist[i] << endl;
+    }
+
+    for(int i = 0; i < N; i++){
+        cout<<"i: "<< i << endl;
+        cout<<"\t"<< endl;
+
+        
+        for(int j = 0; j < M; j++){
+            int bu = G.E[j].u;
+            int bv = G.E[j].v;
+            int bw = G.E[j].w;
+            G.E[j].dprint();
+            cout<< endl;
+            cout<< "T[u]: "<< T_dist[bu]<< ", "<< "T[v]: "<< T_dist[bv] << endl;
+
+            if(T_dist[bu] != inf && T_dist[bu] + bw <= T_dist[bv]){
+                T_dist[bv] = T_dist[bu] + bw;
+            }
+
+
+
+        }
+
+        // for (int i = 0; i < G.e_count(); i++) {
+        //     int bu = G.E[i].u;
+        //     int bv = G.E[i].v;
+        //     int weight = G.E[i].w;
+        //     if (T_dist[bu] != inf
+        //         && T_dist[bu] + weight < T_dist[bv]) {
+        //     }
+ 
+        // }
+    }
+    cout<< endl;
+    for(int i = 0; i < T_dist.size(); i++){
+        cout<< "i: "<<i << " "<< T_dist[i] << endl;
+    }
+
 }
-
-// A utility function to print the constructed distance
-// array
-void printSolution(int dist[])
-{
-	cout << "Vertex \t Distance from Source" << endl;
-	for (int i = 0; i < V; i++)
-		cout << i << " \t\t\t\t" << dist[i] << endl;
-}
-
-// Function that implements Dijkstra's single source
-// shortest path algorithm for a graph represented using
-// adjacency matrix representation
-void dijkstra(int graph[V][V], int src)
-{
-	int dist[V]; // The output array. dist[i] will hold the
-				// shortest
-	// distance from src to i
-
-	bool sptSet[V]; // sptSet[i] will be true if vertex i is
-					// included in shortest
-	// path tree or shortest distance from src to i is
-	// finalized
-
-	// Initialize all distances as INFINITE and stpSet[] as
-	// false
-	for (int i = 0; i < V; i++)
-		dist[i] = INT_MAX, sptSet[i] = false;
-
-	// Distance of source vertex from itself is always 0
-	dist[src] = 0;
-
-	// Find shortest path for all vertices
-	for (int count = 0; count < V - 1; count++) {
-		// Pick the minimum distance vertex from the set of
-		// vertices not yet processed. u is always equal to
-		// src in the first iteration.
-		int u = minDistance(dist, sptSet);
-
-		// Mark the picked vertex as processed
-		sptSet[u] = true;
-
-		// Update dist value of the adjacent vertices of the
-		// picked vertex.
-		for (int v = 0; v < V; v++)
-
-			// Update dist[v] only if is not in sptSet,
-			// there is an edge from u to v, and total
-			// weight of path from src to v through u is
-			// smaller than current value of dist[v]
-			if (!sptSet[v] && graph[u][v]
-				&& dist[u] != INT_MAX
-				&& dist[u] + graph[u][v] < dist[v])
-				dist[v] = dist[u] + graph[u][v];
-	}
-
-	// print the constructed distance array
-	printSolution(dist);
-}
-
-// driver's code
-int main()
-{
-
-	/* Let us create the example graph discussed above */
-	int graph[V][V] = { { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
-						{ 4, 0, 8, 0, 0, 0, 0, 11, 0 },
-						{ 0, 8, 0, 7, 0, 4, 0, 0, 2 },
-						{ 0, 0, 7, 0, 9, 14, 0, 0, 0 },
-						{ 0, 0, 0, 9, 0, 10, 0, 0, 0 },
-						{ 0, 0, 4, 14, 10, 0, 2, 0, 0 },
-						{ 0, 0, 0, 0, 0, 2, 0, 1, 6 },
-						{ 8, 11, 0, 0, 0, 0, 1, 0, 7 },
-						{ 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
-
-	// Function call
-	dijkstra(graph, 0);
-
-	return 0;
-}
-
-// This code is contributed by shivanisinghss2110
---
+++ BellmanFord.hpp
@@ -0,0 +1,15 @@
+#ifndef __BELLMANFORD_HPP__
+#define __BELLMANFORD_HPP__
+
+#include "std_libs.hpp"
+
+class BellmanFord{
+    public:
+        int minDistance(int A[], bool S[]);
+        void printSolution(int A[]);
+
+
+};
+
+
+#endif
